5a3d2e47d2a55023fd347b2ac4d2d5a0263bbfe4
AgustinaGV <agustinagarciavega@gmail.com>
2019-09-19 10:02:05 -0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -210,18 +210,6 @@ function totalRestart() {
     restart();
 }
 
-// functions to save data with localStorage;
-
-function saveData () {
-
-    savedTurn = localStorage.setItem (turn);
-    savedMoves = localStorage.setItem (moves);
-    savedCheck = localStorage.setItem (check);
-    savedX = localStorage.setItem (playerX);
-    savedO = localStorage.setItem (playerO);
-    savedArray = localStorage.setItem (array);
-
-}
 
 
 /*document.getElementById('winDiv').innerHTML = '<h1>¡Empate!</h1> <iframe src="https://giphy.com/embed/xThtajsa65bqiQP32g" width="480" height="391" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>Felicitaciones X y O. Estuvo muy parejo. ¿Van por el desempate?</p><span onclick="reiniciar()">Empezar nueva partida</span>'EMPATE CON EL GIF*/
