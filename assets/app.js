f8deab7c3d36335c7cdd8965b089ddfec7966179
dieguito711 <diegosalischiker711@gmail.com>
2019-09-20 15:24:15 -0300

    Cambios CSS y cambia color a combinación ganadora. Hay que rehacer con código más serio, no con 60 líneas.

--- assets/app.js
+++ assets/app.js
@@ -215,6 +215,53 @@ function checkWin() {
         array[0][0] === check && array[1][1] === check && array[2][2] === check ||
         array[2][0] === check && array[1][1] === check && array[0][2] === check) {
 
+        if (array[0][0] === check && array[0][1] === check && array[0][2]) {
+            document.getElementById("00").classList.add('win');
+            document.getElementById("01").classList.add('win');
+            document.getElementById("02").classList.add('win');
+        } else {
+            if (array[1][0] === check && array[1][1] === check && array[1][2]) {
+                document.getElementById("10").classList.add('win');
+                document.getElementById("11").classList.add('win');
+                document.getElementById("12").classList.add('win');
+            } else {
+                if (array[2][0] === check && array[2][1] === check && array[2][2]) {
+                    document.getElementById("20").classList.add('win');
+                    document.getElementById("21").classList.add('win');
+                    document.getElementById("22").classList.add('win');
+                } else {
+                    if (array[0][0] === check && array[1][0] === check && array[2][0]) {
+                        document.getElementById("00").classList.add('win');
+                        document.getElementById("10").classList.add('win');
+                        document.getElementById("20").classList.add('win');
+                    } else {
+                        if (array[0][1] === check && array[1][1] === check && array[2][1]) {
+                            document.getElementById("01").classList.add('win');
+                            document.getElementById("11").classList.add('win');
+                            document.getElementById("21").classList.add('win');
+                        } else {
+                            if (array[0][2] === check && array[1][2] === check && array[2][2]) {
+                                document.getElementById("02").classList.add('win');
+                                document.getElementById("12").classList.add('win');
+                                document.getElementById("22").classList.add('win');
+                            } else {
+                                if (array[0][0] === check && array[1][1] === check && array[2][2]) {
+                                    document.getElementById("00").classList.add('win');
+                                    document.getElementById("11").classList.add('win');
+                                    document.getElementById("22").classList.add('win');
+                                } else {
+                                    if (array[2][0] === check && array[1][1] === check && array[0][2]) {
+                                        document.getElementById("20").classList.add('win');
+                                        document.getElementById("11").classList.add('win');
+                                        document.getElementById("02").classList.add('win');
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         //WIN
         if (check === "x") {
             playerX++;
@@ -232,7 +279,7 @@ function checkWin() {
 
 function win(check) {
     if (check == "x" || check == "o") {
-        document.getElementById('winDiv').innerHTML = '<h1>¡Ganó el jugador ' + check + '!</h1><span onclick="restart()">Empezar nueva partida</span>';
+        document.getElementById('winDiv').innerHTML = '<h1>¡Ganó el jugador ' + check.toUpperCase() + '!</h1><span onclick="restart()">Empezar nueva partida</span>';
         document.getElementById('winDiv').className = "appear";
     }
 }
@@ -242,7 +289,7 @@ function changeTurn() {
 }
 
 function totalRestart() {
-
+    moves = 0;
     playerX = 0;
     playerO = 0;
 
